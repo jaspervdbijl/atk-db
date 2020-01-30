d97874cb39ffb446acb6c7f03d96fb5cd2780a43
jaspervdbijl <jaspervdb@spsi.co.za>
2020-01-30 11:34:49 +0200

    Integrated firebase messaging

--- src/main/java/com/acutus/atk/db/fe/FEHelper.java
+++ src/main/java/com/acutus/atk/db/fe/FEHelper.java
@@ -14,6 +14,8 @@ import com.acutus.atk.util.Strings;
 import lombok.SneakyThrows;
 import lombok.extern.java.Log;
 import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+
 import javax.persistence.Enumerated;
 import java.sql.*;
 import java.time.temporal.Temporal;
@@ -27,8 +29,7 @@ import static com.acutus.atk.db.constants.EnvProperties.DB_FE_STRICT;
 import static com.acutus.atk.db.sql.SQLHelper.execute;
 import static com.acutus.atk.util.AtkUtil.handle;
 
-
-@Log
+@Slf4j
 public class FEHelper {
 
     public static void maintainDataDefinition(Connection connection, List<Class<? extends AbstractAtkEntity>> classes) {
@@ -77,7 +78,7 @@ public class FEHelper {
     }
 
     private static void logAndExecute(Connection connection, String sql) {
-        log.warning(sql);
+        log.warn(sql);
         execute(connection, sql);
 
     }
