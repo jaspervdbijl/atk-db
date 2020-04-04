f0afff8852080b02a508132fd1cf4a61eaba6683
jaspervdbijl <jaspervdb@spsi.co.za>
2020-04-04 16:08:57 +0200

    Issue 172: change booking date request - client

--- src/main/java/com/acutus/atk/db/Query.java
+++ src/main/java/com/acutus/atk/db/Query.java
@@ -56,7 +56,7 @@ public class Query<T extends AbstractAtkEntity, O> {
     @SneakyThrows
     private String getLeftJoin(AtomicInteger cnt, AbstractAtkEntity entity, AtkEnRelation re) {
         AbstractAtkEntity e = (AbstractAtkEntity) re.getType().getConstructor().newInstance();
-        AtkEnFields fk = e.getEnFields().getForeignKeys(this.entity.getClass());
+        AtkEnFields fk = e.getEnFields().getForeignKeys(entity.getClass());
         String tableName = getTmpTablename(cnt.getAndIncrement());
         Assert.isTrue(fk.size() == 1, "FK [" + fk.getColNames() + "] not located for " + entity.getTableName());
         return String.format("left join %s %s on %s = %s.%s", e.getTableName(), tableName,
@@ -145,7 +145,7 @@ public class Query<T extends AbstractAtkEntity, O> {
         String sql = prepareSql(filter).replaceAll("\\p{Cntrl}", " ");
         // add all the left joins
         sql = sql.substring(sql.toLowerCase().indexOf(" from "));
-        Strings lj = selectFilter == null ? getLeftJoin(new AtomicInteger(0), entity) : new Strings();
+        Strings lj = selectFilter == null && !filter.isCustom() ? getLeftJoin(new AtomicInteger(0), entity) : new Strings();
         Strings split = Strings.asList(sql.replace(",", " , ").split("\\s+"));
         split.add(split.indexOfIgnoreCase(entity.getTableName()) + 1, lj.toString(" "));
         if (!lj.isEmpty()) {
@@ -162,13 +162,17 @@ public class Query<T extends AbstractAtkEntity, O> {
             filter.prepare(ps);
             try (ResultSet rs = ps.executeQuery()) {
                 while (rs.next() && (limit > 0 || limit < 0)) {
-                    Two<AbstractAtkEntity, Boolean> value = loadCascade(new AtomicInteger(-1), map, entity, rs);
-                    if (lastEntity != null &&
-                            !value.getFirst().isIdEqual(lastEntity.getFirst())) {
-                        limit--;
-                        iterate.call((T) lastEntity.getFirst());
+                    if (filter.isCustom()) {
+                        iterate.call((T) entity.set(rs).clone());
+                    } else {
+                        Two<AbstractAtkEntity, Boolean> value = loadCascade(new AtomicInteger(-1), map, entity, rs);
+                        if (lastEntity != null &&
+                                !value.getFirst().isIdEqual(lastEntity.getFirst())) {
+                            limit--;
+                            iterate.call((T) lastEntity.getFirst());
+                        }
+                        lastEntity = value;
                     }
-                    lastEntity = value;
                 }
             }
         }
