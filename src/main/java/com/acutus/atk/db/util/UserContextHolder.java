a0e254d7b3c51f5818e97c323af0bca896dd1492
Jasper van der Bijl <jasper@vanderbijl.co>
2021-05-24 17:17:35 +0200

    https://cellstop.atlassian.net/browse/VD-74

--- src/main/java/com/acutus/atk/db/util/UserContextHolder.java
+++ src/main/java/com/acutus/atk/db/util/UserContextHolder.java
@@ -1,5 +1,11 @@
 package com.acutus.atk.db.util;
 
+import lombok.SneakyThrows;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class UserContextHolder {
 
     private static ThreadLocal<String> USER_LOCAL = new ThreadLocal<>();
@@ -11,4 +17,11 @@ public class UserContextHolder {
     public static String getUsername() {
         return USER_LOCAL.get();
     }
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        System.out.println(Arrays.stream(Files.readString(Path.of("/Users/jaspervdbijl/Downloads/tables.sh")).split("\n")).reduce((s1, s2) -> s1+" " + s2).get());
+    }
+
+
 }
